078e88c891a275d2a5db7f5a64296cb8da08e309
Norman Kuang <nkuang123@gmail.com>
2023-08-29 21:58:59 -0700

    perlin w/ frequency

--- Scenes/Objects/world.gd
+++ Scenes/Objects/world.gd
@@ -1,7 +1,9 @@
 class_name World
 extends Node2D
 
-const MAP_SIZE: Vector2 = Vector2(1920, 1080)
+const MAP_SIZE: Vector2 = Vector2(256, 256)
+const ATLAS_SIZE: Vector2 = Vector2(11, 11)
+const NOISE_FREQUENCY: float = 0.05
 
 var player: Player
 var enemyBase: EnemyBase
@@ -25,22 +27,33 @@ func _ready():
 
 func generate_world():
     var spawn = tilemap.local_to_map(Vector2(0, 0))
-    
     var noise_generator = FastNoiseLite.new()
     noise_generator.set_noise_type(FastNoiseLite.TYPE_PERLIN)
-    noise_generator.seed = randi();
+    noise_generator.set_frequency(NOISE_FREQUENCY)
+#    noise_generator.set_fractal_octaves(10)
+    noise_generator.seed = randi()
 
     for x in MAP_SIZE.x:
         for y in MAP_SIZE.y:
             # MAP_SIZE / 2 to center the map relative to origin (0, 0).
             var coordinates = Vector2i(spawn.x - MAP_SIZE.x / 2 + x, spawn.y - MAP_SIZE.y / 2 + y)
-            # These parameters need tuning. Atlas needs to also be set up for
-            # procedural generation i.e. grass tiles with grass, mountains with mountain,
-            # exclude tower tiles.
-            var noise_value = noise_generator.get_noise_2d(coordinates.x, coordinates.y) * 10
-            var atlas_coordinates = Vector2(round((noise_value * 10) / 5 + 6), round((noise_value * 10) / 5 + 6))
+            # TODO(nkuang): Atlas needs to also be set up for procedural generation i.e. grass
+            # tiles with grass, mountains with mountain, exclude tower tiles.
+
+            # The noise generator returns a value from [-1, 1]. Assuming the
+            # tile atlas is square (N x N), we multiple the noise value by N to
+            # span the entire atlas since the noise value maps to a specific
+            # tile in the atlas.
+            #
+            # Note that most noise functions don't produce values close to the extrema.
+            var noise_value = (noise_generator.get_noise_2dv(coordinates)) * (ATLAS_SIZE.x / 2)
+#            var noise_value = (noise_generator.get_noise_2dv(coordinates)) * 110
+            # Atlas mapping requires positive, integer coordinates.
+            var atlas_x = abs(round(noise_value + (ATLAS_SIZE.x / 2)))
+            var atlas_y = abs(round(noise_value + (ATLAS_SIZE.x / 2)))
+            var atlas_coordinates = Vector2(atlas_x, atlas_y)
             tilemap.set_cell(0, coordinates, 0, atlas_coordinates, 0)
-            
+
 
 func _input(event: InputEvent):
     if event.is_action("escape"):
