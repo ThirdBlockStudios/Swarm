9eb30045b3b5febd5588c693d9276e32d623d18b
Norman Kuang <nkuang123@gmail.com>
2023-09-01 19:59:29 -0700

    [Tilemap] Collisions + new tiles (#12)
    * new tile changes so far
    
    * working colisions

--- Scenes/Objects/world.gd
+++ Scenes/Objects/world.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 const MAP_SIZE: Vector2 = Vector2(256, 256)
 const ATLAS_SIZE: Vector2 = Vector2(11, 11)
-const NOISE_FREQUENCY: float = 0.05
+const NOISE_FREQUENCY: float = 0.04
 
 var player: Player
 var enemyBase: EnemyBase
@@ -37,22 +37,17 @@ func generate_world():
         for y in MAP_SIZE.y:
             # MAP_SIZE / 2 to center the map relative to origin (0, 0).
             var coordinates = Vector2i(spawn.x - MAP_SIZE.x / 2 + x, spawn.y - MAP_SIZE.y / 2 + y)
-            # TODO(nkuang): Atlas needs to also be set up for procedural generation i.e. grass
-            # tiles with grass, mountains with mountain, exclude tower tiles.
-
             # The noise generator returns a value from [-1, 1]. Assuming the
             # tile atlas is square (N x N), we multiple the noise value by N to
             # span the entire atlas since the noise value maps to a specific
             # tile in the atlas.
             #
             # Note that most noise functions don't produce values close to the extrema.
-            var noise_value = (noise_generator.get_noise_2dv(coordinates)) * (ATLAS_SIZE.x / 2)
-#            var noise_value = (noise_generator.get_noise_2dv(coordinates)) * 110
-            # Atlas mapping requires positive, integer coordinates.
-            var atlas_x = abs(round(noise_value + (ATLAS_SIZE.x / 2)))
-            var atlas_y = abs(round(noise_value + (ATLAS_SIZE.x / 2)))
-            var atlas_coordinates = Vector2(atlas_x, atlas_y)
-            tilemap.set_cell(0, coordinates, 0, atlas_coordinates, 0)
+#            var noise_value = (noise_generator.get_noise_2dv(coordinates)) * (ATLAS_SIZE.x / 2)
+            var noise_value = (noise_generator.get_noise_2dv(coordinates)) / 2 + 0.5
+            var atlas_x = 0 if (noise_value < 0.6) else 1
+            var atlas_coordinates = Vector2(atlas_x, 0)
+            tilemap.set_cell(0, coordinates, 2, atlas_coordinates, 0)
 
 
 func _input(event: InputEvent):
